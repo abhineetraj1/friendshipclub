e7ec5b24f4734ecf6aa582e13b3a5844fa4ed206
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2023-04-16 07:12:20 +0530

    Updated code
    -Added flexibility

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import random
 from flask import request
 import datetime as dt
 import smtplib
+import os
+from flask import render_template
 
-app = Flask(__name__, static_folder="files")
+app = Flask(__name__, static_folder="files", template_folder=os.getcwd())
 
+#Creating function to send OTP to mail
 def mail(arg1,arg):
 	av=smtplib.SMTP_SSL("smtp.domain.com",port)
 	av.login("youremail@domain.com","password")
@@ -16,14 +19,12 @@ def mail(arg1,arg):
 		return "Y"
 	except:
 		return "X"
-def web_mg(a):
-	return "<script>alert('"+a+"');</script>"
-def r(a):
-	return open(a,"r+").read()
+
 @app.route("/",methods=["GET","POST"])
 def m():
 	if (request.method=="POST"):
 		name= str(dt.datetime.now()).replace(":","").replace("-","").replace(".","").replace(" ","")
+		#Getting random value for OTP
 		otp= random.choice(["a","b","c","d","e","f","g","9","8","5","3"])+random.choice(["a","b","c","d","e","f","g","9","8","5","3"])+random.choice(["a","b","c","d","e","f","g","9","8","5","3"])+random.choice(["a","b","c","d","e","f","g","9","8","5","3"])+random.choice(["a","b","c","d","e","f","g","9","8","5","3"])+random.choice(["a","b","c","d","e","f","g","9","8","5","3"])
 		os.mkdir("acc/"+name)
 		open("acc/"+name+"/otp","w+").write(otp)
@@ -31,32 +32,31 @@ def m():
 		open("acc/"+name+"/1","a").write("")
 		d= mail(request.form["email"].replace(" ",""),("Subject:OTP verification\nSeems like you have created your form in U KNOW ME\nYour OTP - "+otp))
 		if (d == "X"):
-			return "<script>alert('Enter valid email ID');</script>"+r("index.html")
-		return r("otp.html").replace("f-content",name)
+			return render_template("index.html", msg="Enter valid email ID")
+		return render_template("otp.html", fcontent=name)
 	else:
-		return r("index.html")
+		return render_template("index.html", msg="none")
 
-@app.route("/otp",methods=["GET","POST"])
+@app.route("/otp",methods=["POST"])
 def m_2():
-	if (request.method=="POST"):
-		if (request.form["otp"] == r("acc/"+request.form["name"]+"/otp")):
-			return r("create.html").replace("dfdfdf","127.0.0.1:5000/"+request.form["name"])
-		else:
-			sl.rmtree("acc/"+request.form["name"])
-			return "<script>alert('Wrong otp. Try again');</script>"+r("index.html")
+	#Verifying OTP and creating webpage
+	if (request.form["otp"] == r("acc/"+request.form["name"]+"/otp")):
+		return render_template("create.html", dfdfd="127.0.0.1:5000/"+request.form["name"])
 	else:
-		return ""
+		sl.rmtree("acc/"+request.form["name"])
+		return render_template("index.html",msg="Wrong OTP, try again")
 
 @app.route("/<n>",methods=["GET","POST"])
 def m_1(n):
 	if (request.method=="GET"):
+		#Checking is user exists
 		if (n in os.listdir("acc")):
-			return r("survey.html")+"<script>localStorage.setItem('username','"+n+"');</script>"
+			return render_template("survey.html", username=n)
 		else:
-			return r("404.html")
+			return render_template("404.html")
 	else:
 		mail(r("acc/"+request.form["user"]+"/mail"),("Subject:Response from your friend\n\nName:"+request.form["nameof"]+"\nWhich is your favourite animal? - "+request.form["df1"]+"\nWhat would you like to have with me , when we will meet? - "+request.form["df2"]+"\nWhat will you check, if I will give my mobile to u? - "+request.form["df3"]+"\nDefine me in one word - "+request.form["df4"]+"\nWhere will we go , if we get a chance for trip? - "+request.form["df5"]+"\n\nHope you are enjoying our service!!"))
-		return r("submit.html")
+		return render_template("submit.html")
 
 if "__main__" == __name__:
 	app.run()
